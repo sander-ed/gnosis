7b8cc80530ef19061a59891afcdc92fdc1ed1f48
sander-ed <sander.dahling@sparebank1.no>
2026-09-20 12:08:05 +0200

    Added AGENTS, empty readme and some go code

---
+++ go.mod
@@ -0,0 +1,3 @@
+module gnosis
+
+go 1.27.1
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var i int
+
+	fmt.Print("Type a number: ")
+	fmt.Scan(&i)
+	fmt.Println("Your number is:", i)
+}
